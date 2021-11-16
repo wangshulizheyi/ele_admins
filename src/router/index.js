907d79a2c0ee07288ff716bbece932295f6b83ee
wangshu <18736587312@163.com>
2021-11-16 14:29:01 +0800

    docs(初始化项目环境): 增加esLint pretter git cz约束

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Login'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
